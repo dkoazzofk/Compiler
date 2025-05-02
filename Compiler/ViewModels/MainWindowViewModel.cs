42ee0b438edb8fa927e0dd687263ee5a6641da98
dkoazzofk <153649572+dkoazzofk@users.noreply.github.com>
2025-05-02 16:43:38 +0700

    Add files via upload

---
+++ Compiler/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,482 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Lab5;
+using Lab6;
+using Lab7;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace Compiler;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private IParser _parser;
+    private WhileParser _whileParser;
+
+    private FileManager _fileManager;
+    private ILexicalAnalyzer _lexicalAnalyzer;
+    private WhileLexer _whileLexer;
+
+    private const string _aboutPath = @"Resources\About.html";
+    private const string _helpPath = @"Resources\Help.html";
+    private const string _grammarClassificationPath = @"Resources\GrammarClassification.html";
+    private const string _grammarPath = @"Resources\Grammar.html";
+    private const string _methodOfAnalysisPath = @"Resources\MethodOfAnalysis.html";
+    private const string _literaturePath = @"Resources\ListOfLiterature.html";
+    private const string _neutralizingErrorsPath = @"Resources\NeutralizingErrors.html";
+    private const string _problemStatementPath = @"Resources\ProblemStatement.html";
+    private const string _correctTestCasePath = @"Resources\correct_test_case.txt";
+    private const string _wrongTestCasePath = @"Resources\wrong_test_case.txt";
+
+    private string _currentFilePath;
+    private string _fileContent;
+    private bool _isFileModified;
+
+    private RelayCommand _createNewFileCommand;
+    private RelayCommand _openFileCommand;
+    private RelayCommand _saveFileCommand;
+    private RelayCommand _saveAsFileCommand;
+    private RelayCommand _aboutCommand;
+    private RelayCommand _literatureCommand;
+    private RelayCommand _helpCommand;
+    private RelayCommand _exitCommand;
+    private RelayCommand _problemStatementCommand;
+    private RelayCommand _openTestCaseCommand;
+    private RelayCommand _openWrongTestCaseCommand;
+    private RelayCommand _grammarCommand;
+    private RelayCommand _grammarClassificationCommand;
+    private RelayCommand _neutralizingErrorsCommand;
+    private RelayCommand _methodOfAnalysisCommand;
+    private RelayCommand _startAnalyzersCommand;
+    private RelayCommand _viewSourceCodeCommand;
+    private RelayCommand _removeErrorsCommand;
+    private RelayCommand _parseWhileCommand;
+    private RelayCommand _calculateExprCommand;
+    private RelayCommand _reg1Command;
+    private RelayCommand _reg2Command;
+    private RelayCommand _reg3Command;
+
+    public event EventHandler<StringEventArgs> StringSent;
+    public event EventHandler<Lexeme> LexemeSent;
+    public event EventHandler<ParserError> ErrorSent;
+
+    private List<Lexeme> _lexemesList;
+    private List<Token> _tokensList;
+    private ObservableCollection<Lexeme> _lexemes;
+    private ObservableCollection<ParserError> _incorrectLexemes;
+    private Lexeme _selectedLexeme;
+    private ParserError _selectedError;
+    private string _vmText;
+
+    public event EventHandler RequestClose;
+
+    public ObservableCollection<Lexeme> Lexemes
+    {
+        get { return _lexemes; }
+        set
+        {
+            _lexemes = value;
+            OnPropertyChanged(nameof(Lexemes));
+        }
+    }
+
+    public ObservableCollection<ParserError> IncorrectLexemes
+    {
+        get { return _incorrectLexemes; }
+        set
+        {
+            _incorrectLexemes = value;
+            OnPropertyChanged(nameof(IncorrectLexemes));
+        }
+    }
+
+    public Lexeme SelectedLexeme
+    {
+        get { return _selectedLexeme; }
+        set
+        {
+            _selectedLexeme = value;
+            LexemeSent(this, value);
+            OnPropertyChanged(nameof(SelectedLexeme));
+        }
+    }
+
+    public ParserError SelectedError
+    {
+        get { return _selectedError; }
+        set
+        {
+            _selectedError = value;
+            ErrorSent(this, value);
+            OnPropertyChanged(nameof(SelectedLexeme));
+        }
+    }
+
+    public string FileContent
+    {
+        get { return _fileContent; }
+        set
+        {
+            _fileContent = value;
+            IsFileModified = true;
+            OnPropertyChanged(nameof(FileContent));
+        }
+    }
+
+    public bool IsFileModified
+    {
+        get { return _isFileModified; }
+        set
+        {
+            _isFileModified = value;
+            OnPropertyChanged(nameof(IsFileModified));
+            OnPropertyChanged(nameof(WindowTitle));
+        }
+    }
+
+    public string CurrentFilePath
+    {
+        get { return _currentFilePath; }
+        set
+        {
+            _currentFilePath = value;
+            OnPropertyChanged(nameof(CurrentFilePath));
+            OnPropertyChanged(nameof(WindowTitle));
+        }
+    }
+
+    public string VMText
+    {
+        get { return _vmText; }
+        set
+        {
+            _vmText = value;
+            OnPropertyChanged(nameof(VMText));
+        }
+    }
+
+    public string WindowTitle
+    {
+        get => $"Компилятор — {((CurrentFilePath == string.Empty) ? "Новый файл.txt" : "")}{_currentFilePath.Split(@"\").Last()}{(IsFileModified ? "*" : "")} {((CurrentFilePath != string.Empty) ? "(" : "")}{_currentFilePath}{((CurrentFilePath != string.Empty) ? ")" : "")}";
+    }
+
+    public MainWindowViewModel()
+    {
+        _fileManager = new FileManager();
+        _fileContent = string.Empty;
+        CurrentFilePath = string.Empty;
+        IsFileModified = false;
+        _lexicalAnalyzer = new LexicalAnalyzer();
+        IncorrectLexemes = new ObservableCollection<ParserError>();
+        _whileLexer = new WhileLexer();
+        _whileParser = new WhileParser();
+        _parser = new Parser(string.Empty);
+    }
+
+    public RelayCommand NeutralizingErrorsCommand
+    {
+        get => _neutralizingErrorsCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_neutralizingErrorsPath));
+    }
+    
+    public RelayCommand MethodOfAnalysisCommand
+    {
+        get => _methodOfAnalysisCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_methodOfAnalysisPath));
+    }
+    
+    public RelayCommand GrammarClassificationCommand
+    {
+        get => _grammarClassificationCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_grammarClassificationPath));
+    }
+
+    public RelayCommand GrammarCommand
+    {
+        get => _grammarCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_grammarPath));
+    }
+
+    public RelayCommand ProblemStatementCommand
+    {
+        get => _problemStatementCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_problemStatementPath));
+    }
+
+    public RelayCommand LiteratureCommand
+    {
+        get => _literatureCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_literaturePath));
+    }
+    public RelayCommand CreateNewFileCommand
+    {
+        get => _createNewFileCommand ??= new RelayCommand(CreateNewFile);
+    }
+
+    public RelayCommand ParseWhileCommand
+    {
+        get => _parseWhileCommand ??= new RelayCommand(ParseWhile);
+    }
+    
+    public RelayCommand CalculateExprCommand
+    {
+        get => _calculateExprCommand ??= new RelayCommand(CalculateExpr);
+    }
+
+    public RelayCommand OpenFileCommand
+    {
+        get => _openFileCommand ??= new RelayCommand(OpenFile);
+    }
+
+    public RelayCommand SaveFileCommand
+    {
+        get => _saveFileCommand ??= new RelayCommand(SaveFile, _ => _isFileModified || CurrentFilePath == string.Empty);
+    }
+
+    public RelayCommand SaveAsFileCommand
+    {
+        get => _saveAsFileCommand ??= new RelayCommand(SaveAsFile);
+    }
+    
+    public RelayCommand AboutCommand
+    {
+        get => _aboutCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_aboutPath));
+    }
+    
+    public RelayCommand HelpCommand
+    {
+        get => _helpCommand ??= new RelayCommand(_ => HtmlHelper.OpenInBrowser(_helpPath));
+    }
+
+    public RelayCommand OpenTestCaseCommand
+    {
+        get => _openTestCaseCommand ??= new RelayCommand(OpenTestCase);
+    }
+    
+    public RelayCommand OpenWrongTestCaseCommand
+    {
+        get => _openWrongTestCaseCommand ??= new RelayCommand(OpenWrongTestCase);
+    }
+
+    public RelayCommand ExitCommand
+    {
+        get => _exitCommand ??= new RelayCommand(Exit);
+    }
+
+    public RelayCommand StartAnalyzersCommand
+    {
+        get => _startAnalyzersCommand ??= new RelayCommand(StartAnalysis);
+    }
+    
+    public RelayCommand RemoveErrorsCommand
+    {
+        get => _removeErrorsCommand ??= new RelayCommand(RemoveErrors);
+    }
+
+    public RelayCommand Reg1Command
+    {
+        get => _reg1Command ??= new RelayCommand(FindReg1);
+    }
+
+    public RelayCommand Reg2Command
+    {
+        get => _reg2Command ??= new RelayCommand(FindReg2);
+    }
+
+    public RelayCommand Reg3Command
+    {
+        get => _reg3Command ??= new RelayCommand(FindReg3);
+    }
+
+    public void FindReg1(object obj)
+    {
+        PrintRegResult(RegexExamples.ValidatePhoneNumber(_fileContent));
+    }
+
+    public void FindReg2(object obj)
+    {
+        PrintRegResult(RegexExamples.ValidateFullName(_fileContent));
+    }
+
+    public void FindReg3(object obj)
+    {
+        PrintRegResult(RegexExamples.ValidateLatitude(_fileContent));
+    }
+
+    private void PrintRegResult(List<Match> matches)
+    {
+        VMText = "";
+        foreach (Match match in matches)
+        {
+            VMText += $"Найдено: '{match.Value}', позиция: {match.Index}\n";
+        }
+    }
+
+    public void ParseWhile(object obj)
+    {
+        VMText = _whileParser.Parse(_whileLexer.Analyze(_fileContent));
+    }
+    
+    public void CalculateExpr(object obj)
+    {
+        try
+        {
+            string str = _fileContent.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+            PolishNotationCalculator polishNotationCalculator = new();
+            polishNotationCalculator.Calculate(str);
+            if (polishNotationCalculator.Errors.Count > 0)
+            {
+                VMText = string.Join("\n", polishNotationCalculator.Errors);
+
+            }
+            else
+            {
+                VMText = $"Исходное арифметическое выражение:\n{str}\n\nВыражение в ПОЛИЗ:\n{polishNotationCalculator.Output}\n\nОтвет: {polishNotationCalculator.Result}.";
+            }
+        }
+        catch (Exception ex)
+        {
+            VMText = ex.Message;
+        }
+    }
+
+    public void RemoveErrors(object obj)
+    {
+        StartAnalysis();
+        FileContent = TextCleaner.RemoveIncorrectLexemes(_fileContent, _incorrectLexemes);
+        StartAnalysis();
+
+        SendString(_fileContent);
+    }
+
+    public void OpenTestCase(object obj)
+    {
+        CurrentFilePath = _correctTestCasePath;
+        ReadFileContent();
+    }
+
+    public void OpenWrongTestCase(object obj)
+    {
+        CurrentFilePath = _wrongTestCasePath;
+        ReadFileContent();
+    }
+
+    public void StartAnalysis(object obj = null)
+    {
+        LexicalAnalysis();
+        Parsing();
+    }
+
+    public void LexicalAnalysis()
+    {
+        _lexemesList = _lexicalAnalyzer.Analyze(FileContent);
+
+        Lexemes = new ObservableCollection<Lexeme>(_lexemesList);
+    }
+
+    public void Parsing()
+    {
+        IncorrectLexemes = new ObservableCollection<ParserError>(_parser.Parse(FileContent));
+    }
+
+    public void Exit(object obj = null)
+    {
+        if (CancelOperationAfterCheckingForUnsavedChanges())
+            return;
+
+        OnRequestClose();
+    }
+
+    public void CreateNewFile(object obj)
+    {
+        if (CancelOperationAfterCheckingForUnsavedChanges())
+            return;
+
+        FileContent = string.Empty;
+        SendString(_fileContent);
+        CurrentFilePath = string.Empty;
+        IsFileModified = true;
+    }
+
+    public void OpenFile(object obj = null)
+    {
+        if (CancelOperationAfterCheckingForUnsavedChanges())
+            return;
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        if (openFileDialog.ShowDialog() == true)
+        {
+            _currentFilePath = openFileDialog.FileName;
+            ReadFileContent();
+        }
+    }
+
+    public void SaveAsFile(object obj = null)
+    {
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.Filter = "Текстовый файл (*.txt)|*.txt|C# файл (*.cs)|*.cs|C файл (*.c)|*.c|C++ файл (*.cpp)|*.cpp|Python файл (*.py)|*.py|JavaScript файл (*.js)|*.js|HTML файл (*.html)|*.html|CSS файл (*.css)|*.css|XML файл (*.xml)|*.xml|JSON файл (*.json)|*.json|Markdown файл (*.md)|*.md|PHP файл (*.php)|*.php|Java файл (*.java)|*.java|Все файлы (*.*)|*.*";
+
+        if (saveFileDialog.ShowDialog() == true)
+        {
+            _currentFilePath = saveFileDialog.FileName;
+            _fileManager.SaveFile(_currentFilePath, FileContent);
+            IsFileModified = false;
+        }
+    }
+
+    public void SaveFile(object obj = null)
+    {
+        if (string.IsNullOrEmpty(_currentFilePath))
+        {
+            SaveAsFile();
+        }
+        else
+        {
+            _fileManager.SaveFile(_currentFilePath, FileContent);
+            IsFileModified = false;
+        }
+    }
+
+    public void SendString(string message)
+    {
+        if (StringSent != null)
+        {
+            StringSent(this, new StringEventArgs(message));
+        }
+    }
+
+    public void ReadFileContent()
+    {
+        FileContent = _fileManager.OpenFile(_currentFilePath);
+        SendString(_fileContent);
+        IsFileModified = false;
+    }
+
+    public void HandleDroppedFiles(string[] files)
+    {
+        if (CancelOperationAfterCheckingForUnsavedChanges())
+            return;
+
+        CurrentFilePath = files[0];
+        ReadFileContent();
+    }
+
+    public bool CancelOperationAfterCheckingForUnsavedChanges()
+    {
+        if (_isFileModified)
+        {
+            var result = MessageBoxHelper.ShowUnsavedChangesMessage();
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    SaveFile();
+                    return false;
+                case MessageBoxResult.Cancel:
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnRequestClose()
+    {
+        RequestClose?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Compiler/Views/MainWindow.xaml.cs
@@ -0,0 +1,308 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Xml;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Highlighting;
+using System.IO;
+using System.Windows.Media.Animation;
+
+namespace Compiler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainWindowViewModel viewModel = new MainWindowViewModel();
+            viewModel.StringSent += OnStringReceived;
+            viewModel.LexemeSent += OnLexemeReceived;
+            viewModel.ErrorSent += OnErrorReceived;
+            viewModel.RequestClose += (sender, e) => Close();
+            Closing += MainWindow_Closing;
+
+            DataContext = viewModel;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader s = new StreamReader(@"Resources\SQL.xshd"))
+            {
+                using (XmlTextReader reader = new XmlTextReader(s))
+                {
+                    textEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (((MainWindowViewModel)DataContext).IsFileModified)
+            {
+                if (MessageBoxHelper.ShowWindowClosingMessage() == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        public void OnStringReceived(object sender, StringEventArgs e)
+        {
+            textEditor.Document.Text = e.Message;
+        }
+
+        public void OnLexemeReceived(object sender, Lexeme e)
+        {
+            if (e != null && e.EndIndex <= textEditor.Document.Text.Length)
+            {
+                textEditor.Select(e.StartIndex - 1, e.EndIndex - e.StartIndex + 1);
+            }
+        }
+
+        public void OnErrorReceived(object sender, ParserError e)
+        {
+            if (e != null && e.EndIndex <= textEditor.Document.Text.Length)
+            {
+                textEditor.Select(e.StartIndex - 1, e.EndIndex - e.StartIndex + 1);
+            }
+        }
+
+        private void CutSelectedText(object sender, RoutedEventArgs e)
+        {
+            if (textEditor.SelectionLength > 0)
+            {
+                Clipboard.SetText(textEditor.SelectedText);
+                textEditor.Document.Remove(textEditor.SelectionStart, textEditor.SelectionLength);
+            }
+        }
+
+        private void CopySelectedText(object sender, RoutedEventArgs e)
+        {
+            if (textEditor.SelectionLength > 0)
+            {
+                Clipboard.SetText(textEditor.SelectedText);
+            }
+        }
+
+        private void PasteText(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                textEditor.Document.Insert(textEditor.CaretOffset, Clipboard.GetText());
+            }
+        }
+
+        private void DeleteSelectedText(object sender, RoutedEventArgs e)
+        {
+            if (textEditor.SelectionLength > 0)
+            {
+                textEditor.Document.Remove(textEditor.SelectionStart, textEditor.SelectionLength);
+            }
+        }
+
+        private void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            textEditor.SelectAll();
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files != null && files.Length > 0)
+            {
+                (DataContext as MainWindowViewModel)?.HandleDroppedFiles(files);
+            }
+        }
+
+        private void GetCaretPosition()
+        {
+            int offset = textEditor.CaretOffset;
+            var location = textEditor.Document.GetLocation(offset);
+            //CursorPositionTextBlock.Text = $"Строка: {location.Line}, Столбец: {location.Column}";
+        }
+
+        private void textEditor_TextChanged(object sender, EventArgs e)
+        {
+            undoButton.IsEnabled = textEditor.CanUndo;
+            redoButton.IsEnabled = textEditor.CanRedo;
+
+            undoMenuItem.IsEnabled = textEditor.CanUndo;
+            redoMenuItem.IsEnabled = textEditor.CanRedo;
+
+            GetCaretPosition();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            popupMenu.IsOpen = true;
+        }
+
+        private void textEditor_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            GetCaretPosition();
+        }
+
+        private void textEditor_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            GetCaretPosition();
+        }
+
+        private void undoButton_Click(object sender, RoutedEventArgs e)
+        {
+            textEditor.Undo();
+        }
+
+        private void redoButton_Click(object sender, RoutedEventArgs e)
+        {
+            textEditor.Redo();
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                try
+                {
+                    int fontSize = Convert.ToInt32(fontSizeComboBox.Text);
+                    textEditor.FontSize = fontSize;
+                    parserDataGrid.FontSize = fontSize;
+                    lexerDataGrid.FontSize = fontSize;
+                    outputTextBlock.FontSize = fontSize;
+                }
+                catch { }
+            }));
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+            {
+                ((MainWindowViewModel)(DataContext)).StartAnalyzersCommand.Execute(null);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.F6 && errorStackPanel.Visibility == Visibility.Visible)
+            {
+                ((MainWindowViewModel)(DataContext)).RemoveErrorsCommand.Execute(null);
+                e.Handled = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("""
+                    Для грамматики G[<While>] разработать и реализовать алгоритм анализа на основе метода рекурсивного спуска.
+                
+                G[<While>]:
+                1. <While> → while <Cond> do <Stmt> end ;
+                2. <Cond> → <LogExpr> {or <LogExpr>}
+                3. <LogExpr> → <RelExpr> {and <RelExpr>}
+                4. <RelExpr> → <Operand> [rel <Operand>]
+                5. <Operand> → var | const
+                6. <Stmt> → var as <ArithExpr>
+                7. <ArithExpr> → <Operand> {ao <Operand>}
+
+                    Примечание: while, do, end, and, or – ключевые слова. В тип rel объединили операции сравнения <,<=, >=, >, != и ==, в тип ao арифметические операции + и -, в тип as оператор присваивания =, тип var – название переменной (только буквы), тип const – числа. Причина, по которой не объединены в один тип логические операции and и or заключается в том, что эти операции имеют различный приоритет. Пример цепочки: while a < b and b <= c do b=b+c-20 end;
+                """, "Задание", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textEditor.Text = """
+                while a < b do b = b - 20 end;
+                while a < b and c != d or e == f or g <= h and i > j do abc = cde - 20 + 40 - 8 + efg end;
+                while a < b do b = a + ? end;
+                """;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("""
+                Цель работы: дополнить анализатор, разработанный в рамках лабораторных работ, этапом формирования внутренней формы представления программы.
+
+                Задание: реализовать алгоритм записи арифметического выражения в ПОЛИЗ и алгоритм вычисления выражения в ПОЛИЗ.
+
+                Поддерживаемые операции:
+                1. "(" — открывающаяся скобка;
+                2. ")" — закрывающаяся скобка;
+                3. "+" — оператор сложения;
+                4. "-" — оператор минуса;
+                5. "*" — оператор умножения;
+                6. "/" — оператор деления;
+                7. "^" — оператор возведения в степень.
+                """, "Задание", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            textEditor.Text = """
+                5 + 3 * 8 / 40
+                """;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("""
+                Построить РВ для валидации российских номеров городских телефонов (7 цифр, разделенных пробелами или символами -, без кода города).
+                """, "Задание", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("""
+                Построить РВ, описывающее ФИО человека на русском языке (фамилия, имя и отчество полностью).                
+                """, "Задание", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("""
+                Построить РВ, описывающее широту (учесть диапазон корректных значений).                                
+                """, "Задание", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            textEditor.Text = """
+                Профессор Иванов сидел за своим столом, разбирая старые записи.
+                На одной из бумажек он заметил номер телефона: 123 45 67.
+                "Интересно, чей это номер?" - подумал он.
+                Перебирая другие записи, он нашел еще несколько номеров: 888-99-00, 147-85-20.
+                Похоже, это номера его старых знакомых, которых он давно не видел.
+                """;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            textEditor.Text = """
+                В библиотеке было тихо и спокойно. Иван Петрович Бунин сидел за столом и писал письмо своему другу
+                Алексею Максимовичу Пешкову, более известному как Максим Горький. Он описывал события прошедшей
+                недели и делился своими мыслями о новом романе Анны Андреевны Ахматовой.
+                """;
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            textEditor.Text = """
+                Координаты: -90, 0.5, 45, -10.23, 89.9, 100, -91.9
+                """;
+        }
+    }
+}
